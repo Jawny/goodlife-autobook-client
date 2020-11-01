ea6305c0ac7726a205a8a5678a4f5252dfd1c725
Johnny <johnnyliao06@gmail.com>
2020-10-31 22:42:55 -0700

    build basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
+import React, { useState } from "react";
+import "./App.css";
+
+const axios = require("axios");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [hour, setHour] = useState("");
+
+  function handleUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleYear(e) {
+    setYear(e.target.value);
+  }
+
+  function handleMonth(e) {
+    setMonth(e.target.value);
+  }
+
+  function handleDay(e) {
+    setDay(e.target.value);
+  }
+
+  function handleHour(e) {
+    setHour(e.target.value);
+  }
+  let data = JSON.stringify({
+    username,
+    password,
+    year,
+    month,
+    day,
+    hour,
+  });
+
+  function onSubmit() {
+    axios.post("http://localhost:8080/", data, {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="email" placeholder="username" onChange={handleUsername} />
+      <input type="password" placeholder="password" onChange={handlePassword} />
+      <input type="text" placeholder="Year" onChange={handleYear} />
+      <input type="text" placeholder="Month" onChange={handleMonth} />
+      <input type="text" placeholder="Day" onChange={handleDay} />
+      <input type="text" placeholder="Hour" onChange={handleHour} />
+      <Button onClick={onSubmit}>Submit</Button>
     </div>
   );
 }
