f17ba8b34bf96e0645d22d5aeb6a62569fc2f356
Johnny Liao <johnny@Johnnys-MacBook-Pro.local>
2020-11-08 02:09:44 -0800

    added rough monday-friday functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Button } from "antd";
 import React, { useState } from "react";
+import { Select } from "antd";
+import "antd/dist/antd.css";
 import "./App.css";
 
 const axios = require("axios");
@@ -7,10 +9,43 @@ const axios = require("axios");
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  // const [year, setYear] = useState("");
-  // const [month, setMonth] = useState("");
-  const [day, setDay] = useState("");
-  const [hour, setHour] = useState("");
+  const [monday, setMonday] = useState(0);
+  const [tuesday, setTuesday] = useState(0);
+  const [wednesday, setWednesday] = useState(0);
+  const [thursday, setThursday] = useState(0);
+  const [friday, setFriday] = useState(0);
+  const [saturday, setSaturday] = useState(0);
+  const [sunday, setSunday] = useState(0);
+
+  const { Option } = Select;
+  const weekday = [
+    { value: null, text: "None" },
+    { value: 6, text: "6:00AM - 7:00AM" },
+    { value: 7.5, text: "7:30AM - 8:00AM" },
+    { value: 9, text: "9:00AM - 10:00AM" },
+    { value: 10.5, text: "10:30AM - 11:30AM" },
+    { value: 12, text: "12:00PM - 1:00PM" },
+    { value: 13.5, text: "1:30PM - 2:30PM" },
+    { value: 15, text: "3:00PM - 4:00PM" },
+    { value: 16.5, text: "4:30PM - 5:30PM" },
+    { value: 18, text: "6:00PM - 7:00PM" },
+    { value: 19.5, text: "7:30PM - 8:30PM" },
+    { value: 21, text: "9:00PM - 10:00PM" },
+    { value: 22.5, text: "10:30PM - 11:30PM" },
+  ];
+
+  const weekend = [
+    { value: null, text: "None" },
+    { value: 6, text: "7:00AM - 8:00AM" },
+    { value: 7.5, text: "8:30AM - 9:00AM" },
+    { value: 9, text: "10:00AM - 11:00AM" },
+    { value: 10.5, text: "11:30AM - 12:30PM" },
+    { value: 12, text: "1:00PM - 2:00PM" },
+    { value: 13.5, text: "2:30PM - 3:30PM" },
+    { value: 15, text: "4:00PM - 5:00PM" },
+    { value: 16.5, text: "5:30PM - 6:30PM" },
+    { value: 18, text: "7:00PM - 8:00PM" },
+  ];
 
   function handleUsername(e) {
     setUsername(e.target.value);
@@ -20,28 +55,50 @@ function App() {
     setPassword(e.target.value);
   }
 
-  // function handleYear(e) {
-  //   setYear(e.target.value);
-  // }
+  function handleMonday(e) {
+    setMonday(e);
+    console.log(e);
+  }
 
-  // function handleMonth(e) {
-  //   setMonth(e.target.value);
-  // }
+  function handleTuesday(e) {
+    setTuesday(e);
+    console.log(e);
+  }
 
-  // function handleDay(e) {
-  //   setDay(e.target.value);
-  // }
+  function handleWednesday(e) {
+    setWednesday(e);
+    console.log(e);
+  }
 
-  function handleHour(e) {
-    setHour(e.target.value);
+  function handleThursday(e) {
+    setThursday(e);
+    console.log(e);
+  }
+
+  function handleFriday(e) {
+    setFriday(e);
+    console.log(e);
+  }
+
+  function handleSaturday(e) {
+    setSaturday(e);
+    console.log(e);
+  }
+
+  function handleSunday(e) {
+    setSunday(e);
+    console.log(e);
   }
   let data = JSON.stringify({
     username,
     password,
-    // year,
-    // month,
-    // day,
-    hour,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday,
   });
 
   function onSubmit() {
@@ -54,10 +111,63 @@ function App() {
     <div className="App">
       <input type="email" placeholder="username" onChange={handleUsername} />
       <input type="password" placeholder="password" onChange={handlePassword} />
-      {/* <input type="text" placeholder="Year" onChange={handleYear} />
-      <input type="text" placeholder="Month" onChange={handleMonth} />
-      <input type="text" placeholder="Day" onChange={handleDay} /> */}
-      <input type="text" placeholder="Hour" onChange={handleHour} />
+      <div>
+        <h2>Monday</h2>
+        <Select defaultValue="None" onChange={handleMonday}>
+          {weekday.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Tuesday</h2>
+        <Select defaultValue="None" onChange={handleTuesday}>
+          {weekday.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Wednesday</h2>
+        <Select defaultValue="None" onChange={handleWednesday}>
+          {weekday.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Thursday</h2>
+        <Select defaultValue="None" onChange={handleThursday}>
+          {weekday.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Friday</h2>
+        <Select defaultValue="None" onChange={handleFriday}>
+          {weekday.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Saturday</h2>
+        <Select defaultValue="None" onChange={handleSaturday}>
+          {weekend.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+      <div>
+        <h2>Sunday</h2>
+        <Select defaultValue="None" onChange={handleSunday}>
+          {weekend.map((data) => {
+            return <Option value={data.value}>{data.text}</Option>;
+          })}
+        </Select>
+      </div>
+
       <Button onClick={onSubmit}>Submit</Button>
     </div>
   );
--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,76 +1,71 @@
-import React, { Component } from "react";
+import React from "react";
 import { Select } from "antd";
 import "antd/dist/antd.css";
 
-class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { times: [1, 2, 3] };
-    this.handleChange = this.handleChange.bind(this);
+function Dropdown(props) {
+  const weekday = [
+    { value: null, text: "None" },
+    { value: 6, text: "6:00AM - 7:00AM" },
+    { value: 7.5, text: "7:30AM - 8:00AM" },
+    { value: 9, text: "9:00AM - 10:00AM" },
+    { value: 10.5, text: "10:30AM - 11:30AM" },
+    { value: 12, text: "12:00PM - 1:00PM" },
+    { value: 13.5, text: "1:30PM - 2:30PM" },
+    { value: 15, text: "3:00PM - 4:00PM" },
+    { value: 16.5, text: "4:30PM - 5:30PM" },
+    { value: 18, text: "6:00PM - 7:00PM" },
+    { value: 19.5, text: "7:30PM - 8:30PM" },
+    { value: 21, text: "9:00PM - 10:00PM" },
+    { value: 22.5, text: "10:30PM - 11:30PM" },
+  ];
+
+  const weekend = [
+    { value: null, text: "None" },
+    { value: 6, text: "7:00AM - 8:00AM" },
+    { value: 7.5, text: "8:30AM - 9:00AM" },
+    { value: 9, text: "10:00AM - 11:00AM" },
+    { value: 10.5, text: "11:30AM - 12:30PM" },
+    { value: 12, text: "1:00PM - 2:00PM" },
+    { value: 13.5, text: "2:30PM - 3:30PM" },
+    { value: 15, text: "4:00PM - 5:00PM" },
+    { value: 16.5, text: "5:30PM - 6:30PM" },
+    { value: 18, text: "7:00PM - 8:00PM" },
+  ];
+
+  const { Option } = Select;
+  const { date, day } = props;
+
+  function handle(e) {
+    console.log("changed");
   }
 
-  handleChange(e) {
-    console.log(e);
-    this.setState({
-      times: [...this.state.times, e],
-    });
-    console.log(this.state.times);
-  }
+  const functionHandler = (e) => {
+    props.passChildData(e.target.value);
+  };
 
-  render() {
-    const weekday = [
-      { value: null, text: "None" },
-      { value: 6, text: "6:00AM - 7:00AM" },
-      { value: 7.5, text: "7:30AM - 8:00AM" },
-      { value: 9, text: "9:00AM - 10:00AM" },
-      { value: 10.5, text: "10:30AM - 11:30AM" },
-      { value: 12, text: "12:00PM - 1:00PM" },
-      { value: 13.5, text: "1:30PM - 2:30PM" },
-      { value: 15, text: "3:00PM - 4:00PM" },
-      { value: 16.5, text: "4:30PM - 5:30PM" },
-      { value: 18, text: "6:00PM - 7:00PM" },
-      { value: 19.5, text: "7:30PM - 8:30PM" },
-      { value: 21, text: "9:00PM - 10:00PM" },
-      { value: 22.5, text: "10:30PM - 11:30PM" },
-    ];
-
-    const weekend = [
-      { value: null, text: "None" },
-      { value: 6, text: "7:00AM - 8:00AM" },
-      { value: 7.5, text: "8:30AM - 9:00AM" },
-      { value: 9, text: "10:00AM - 11:00AM" },
-      { value: 10.5, text: "11:30AM - 12:30PM" },
-      { value: 12, text: "1:00PM - 2:00PM" },
-      { value: 13.5, text: "2:30PM - 3:30PM" },
-      { value: 15, text: "4:00PM - 5:00PM" },
-      { value: 16.5, text: "5:30PM - 6:30PM" },
-      { value: 18, text: "7:00PM - 8:00PM" },
-    ];
-    const { Option } = Select;
-    return (
-      <div>
-        {this.props.date === "weekday" ? (
-          <div>
-            <h2>{this.props.day}</h2>
-            <Select defaultValue="None" onChange={this.handleChange}>
-              {weekday.map((data) => {
-                return <Option value={data.value}>{data.text}</Option>;
-              })}
-            </Select>
-          </div>
-        ) : (
-          <div>
-            <h2>{this.props.day}</h2>
-            <Select defaultValue="None" onChange={this.handleChange}>
-              {weekend.map((data) => {
-                return <Option value={data.value}>{data.text}</Option>;
-              })}
-            </Select>
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {date === "weekday" ? (
+        <div>
+          <h2>{day}</h2>
+          <Select defaultValue="None" onChange={functionHandler}>
+            {weekday.map((data) => {
+              return <Option value={data.value}>{data.text}</Option>;
+            })}
+          </Select>
+        </div>
+      ) : (
+        <div>
+          <h2>{day}</h2>
+          <Select defaultValue="None">
+            {weekend.map((data) => {
+              return <Option value={data.value}>{data.text}</Option>;
+            })}
+          </Select>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Dropdown;
