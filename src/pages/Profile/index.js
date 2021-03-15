579f3f0ad9e6f16a26ea9ea43cedfc15a777f064
Johnny Liao <johnnyliao06@gmail.com>
2021-03-14 19:16:54 -0700

    added faq

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -14,6 +14,7 @@ import {
 import Dropdown from "../../components/Dropdown/Dropdown";
 import Loading from "../../components/Loading/Loading";
 import ProfileDetails from "../../components/ProfileDetails";
+import FAQ from "../../components/FAQ";
 import "antd/dist/antd.css";
 import "./Profile.css";
 
@@ -227,6 +228,7 @@ const Profile = () => {
 
         {loading ? <Loading /> : <></>}
       </div>
+      <FAQ />
     </div>
   );
 };
