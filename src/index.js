6cba5641c69db921dee3dd21ab77a852fc36ab4f
Johnny Liao <johnnyliao06@gmail.com>
2021-03-18 19:33:28 -0700

    integrated basic google analytics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
 import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
 
@@ -8,8 +10,19 @@ import Router from "./router";
 import i18n from "./translation";
 import * as serviceWorker from "./serviceWorker";
 
+const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS; // Replace with your Google Analytics tracking ID
+
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter history={history}>
     <Auth0ProviderWithHistory>
       {/* <I18nextProvider i18n={i18n}> */}
       <Router />
